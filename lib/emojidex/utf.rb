7d28c12ab8e9a1878d3703f768a44f861fd3fbdd
kagilinn <kagilinn@gmail.com>
2013-10-09 17:40:39 +0900

    added method 'Emojidex::UTF#emojify()'

--- lib/emojidex/utf.rb
+++ lib/emojidex/utf.rb
@@ -6,13 +6,49 @@ module Emojidex
     attr_reader :list
 
     def initialize
-      file = File.join(File.dirname(File.expand_path(__FILE__)),
+      json_path = File.join(File.dirname(File.expand_path(__FILE__)),
                        './utf/utf-emoji.json')
-      p file.class
-      @list = JSON.parse file
+      @list = JSON.parse(IO.read(json_path))
+      @lookup_unicode, @lookup_name = {}, {}
+      @list.each do |emoji|
+        emoji['xml'] = %|<emoji name="#{emoji['name']}" />|
+        @lookup_unicode[emoji['moji']] = emoji
+        @lookup_name[emoji['name']] = emoji
+      end
     end
 
     def where(options = {})
+    end
+
+    def emojify(src_str)
+      src_str.lines.map {|line|
+        emojify_unicode(emojify_tag(line))
+      }.join("\n")
+    end
+
+private
+    def emojify_unicode(src_str)
+      return src_str.chars.map {|c|
+        emoji = @lookup_unicode[c]
+        emoji ? emoji['xml'] : c
+      }.join('')
+    end
+
+    def emojify_tag(src_str)
+      result, s = '', src_str
+      while s =~ /^([^:]*)\:([^:]+)\:(.*)$/o
+        result += $1
+        emoji_name, rest = $2, $3
+        emoji = @lookup_name[emoji_name]
+        if emoji
+          result += emoji['xml']
+          s = rest
+        else
+          result += ':'
+          s = emoji_name + ':' + rest
+        end
+      end
+      return result
     end
   end
 end
