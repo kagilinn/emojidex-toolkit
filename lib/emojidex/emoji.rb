d57546369bb0b797366bdf62b0ea36d682c1c853
kagilinn <kagilinn@gmail.com>
2013-10-11 01:21:56 +0900

    add to_s method (returns a emoji-tag) to Emoji

--- lib/emojidex/emoji.rb
+++ lib/emojidex/emoji.rb
@@ -3,10 +3,12 @@ module Emojidex
     attr_reader :name
     attr_reader :unicode
     attr_reader :xml
+    attr_reader :to_s
     def initialize(emoji_data)    # Hash from utf/utf-emoji.json
       @name = emoji_data['name']
       @unicode = emoji_data['moji']
       @xml = %|<emoji name="#{name}" />|
+      @to_s = ":#{name}:"
     end
   end
 end
